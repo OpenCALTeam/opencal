016da1912534b65a3bc0c18b4aee8467861e3af3
WS-Alessio <ws-alessio@wsalessio.lan>
2016-09-05 10:54:51 +0200

    Serial 2D Suinos algorithm ported into the master branch

--- OpenCAL/include/OpenCAL/calCommon.h
+++ OpenCAL/include/OpenCAL/calCommon.h
@@ -19,7 +19,7 @@
  * You should have received a copy of the GNU Lesser General Public
  * License along with OpenCAL. If not, see <http://www.gnu.org/licenses/>.
  */
-
+ 
 #ifndef calCommon_h
 #define calCommon_h
 
